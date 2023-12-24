da523d5d99c8ed7e8ff157e13be55b8cd55dccbc
Sahar-Yousef <Sell.lla.78@gmail.com>
2023-12-24 10:56:52 +0300

    First

---
+++ Hmmz/Hmmz/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hmmz
+//
+//  Created by Sahora on 24/12/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
